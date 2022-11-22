98618dfb04e2425d1af368adf3e3b8c3a6a8855e
Sandagomi <sandagomitk@gmail.com>
2022-11-22 20:30:49 +0530

    Get function Completion

--- index.js
+++ index.js
@@ -2,37 +2,68 @@ const PORT = 8000
 const express = require ('express') // importing library
 const axios = require ('axios') // importing library
 const cheerio = require ('cheerio') // importing library
+const { response } = require('express')
 
 const app = express() //initiating and calling the express function
 
+const newspapers = [
+    {
+        name: 'times',
+        address: 'https://www.thetimes.co.uk/environment',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk'
+    }
+
+]
+
 const articles = []
 
-app.get('/', (req,res) => {
-    res.json('Welcome to my climate change API')
-}) // caling get function
 
+newspapers.forEach(newspaper => {
 
-app.get('/news', (req,res) => {
-
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-        .then((response) => {
+    axios.get(newspaper.address)
+        .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
 
             $('a:contains("climate")', html).each(function () {
+
                 const title = $(this).text()
                 const url = $(this).attr('href')
 
+
                 articles.push({
                     title,
-                    url
+                    url: newspaper.base + url,
+                    source: newspaper.name
                 })
+
             })
 
-            res.json(articles)
-        }).catch((err) => console.log(err))
+        })
 
 })
+
+
+app.get('/', (req,res) => {
+    res.json('Welcome to my climate change API')
+}) // calling the get function
+
+
+app.get('/news', (req,res) => {
+
+    res.json(articles)
+
+}) // main get function which calling and scraping the data from websites
 
 
 
