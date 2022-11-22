76b4eb5657795a20ef5876c9290a41162c9c6386
Sandagomi <sandagomitk@gmail.com>
2022-11-22 20:59:12 +0530

    deploying changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+const PORT = process.env.PORT || 8000 // for deploying in heroku
 const express = require ('express') // importing library
 const axios = require ('axios') // importing library
 const cheerio = require ('cheerio') // importing library
